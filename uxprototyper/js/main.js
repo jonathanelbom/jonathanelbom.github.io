0a7a5de13ccaf8db8945db4e76f8aa2d3470d40e
jonathan elbom <jb@jonnybomb.com>
2016-06-13 21:21:41 -0500

    prototype pages done

--- uxprototyper/js/main.js
+++ uxprototyper/js/main.js
@@ -12,15 +12,21 @@
 	var modalClose = document.querySelector( '.modal__close' );
 	var modalCancel = document.querySelector( '.modal__footer__cancel' );
 	var modalNext = document.querySelector( '.modal__footer__next' );
+	var modalBack = document.querySelector( '.modal__footer__back' );
+	var modalBackWrap = document.querySelector( '.modal__footer__back__wrap' );
 	var modalBg = document.querySelector( '.modal__bg' );
 	var cloneCards = [ document.querySelector( '#clone_card_1' ), document.querySelector( '#clone_card_2' ), document.querySelector( '#clone_card_3' ) ];
 	var selectOpts = document.querySelectorAll('.modal__body__content1__select');
-
+	var modalHeader = document.querySelector('.modal__header');
+	var modalFooter = document.querySelector('.modal__footer');
+	var modalStep1 = document.querySelector('.modal__body__content1');
+	var modalStep2 = document.querySelector('.modal__body__content2');
 	var modalOpen = false;
 	var i;
+	
+	// add select clicks for the modal content step 1
 	for (i=0; i<selectOpts.length; i++) {
-		var elem = selectOpts[i];
-		elem.addEventListener('click', toggleResourceRights);
+		selectOpts[i].addEventListener('click', toggleResourceRights);
 	}
 	// clone card for main page
 	for (i=0; i<8; i++) {
@@ -28,6 +34,28 @@
 		clone.id = '';
 		(i%2 === 0 ? colLeft : colRight).appendChild(clone);
 	}
+	// font size handlers
+	document.querySelector('#fontPlus').addEventListener( 'click', function() {
+		updateFontSize('plus');
+	});
+	document.querySelector('#fontMinus').addEventListener( 'click', function() {
+		updateFontSize('minus');
+	});
+
+	function updateFontSize( dir ) {
+		var fontSize = parseInt( getElemStyle( document.body, 'fontSize' ), 0);
+		var newSize = fontSize;
+		if ( dir === 'minus' && fontSize > 8) { //min size
+			newSize -= 1;
+		} else if ( dir === 'plus' && fontSize < 24) { //max size
+			newSize += 1;
+		}
+		if ( newSize !== fontSize ) {
+			document.body.style.fontSize = newSize+'px';
+			resize();
+		}
+	}
+
 	// main action listener
 	addResource.addEventListener( 'click', function() {
 		toggleModal(true);
@@ -39,8 +67,62 @@
 	modalCancel.addEventListener( 'click', function() {
 		toggleModal(false);
 	});
-	//modalNext.addEventListener('click', nextContent);
-	function gotoStep(step) {
+	modalNext.addEventListener('click', function() {
+		gotoStep(2);
+	});
+	modalBack.addEventListener('click', function() {
+		gotoStep(1);
+	});
+	modalDialog.addEventListener( 'transitionend', function(e) {
+		modalOpen = !modalOpen;
+		//console.log('transend, modalOpen:',modalOpen);
+		if ( !modalOpen ) {
+			// reset modal and go back to step 1
+			modalBody.onscroll = null;
+			modalBody.scrollTop = 0;
+			modalScrolling();
+			gotoStep(1);
+			// reset step 1 state
+			for (i=0; i<selectOpts.length; i++) {
+				var elem = selectOpts[i];
+				elem.setAttribute('aria-pressed', 'false');
+				s = elem.querySelector('.select-text');
+				s.textContent = 'Select';
+			}
+			modalNext.setAttribute('disabled', 'disabled');
+			modal.style.display = 'none';
+			modalBg.style.display = 'none';
+		} else {
+			modalBody.onscroll = modalScrolling;
+			modalScrolling();
+		}
+	});
+	function gotoStep( step ) {
+		modalBody.scrollTop = 0;
+		modalScrolling();
+		if ( step === 1 ) {
+			modalDialog.classList.remove('progress');
+			modalBackWrap.classList.add('hidden');
+			modalStep1.style.display = 'block';
+			modalStep2.style.display = 'none';
+			modalNext.removeAttribute('disabled');
+		} else {
+			// step 2
+			modalDialog.classList.add('progress');
+			modalBackWrap.classList.remove('hidden');
+			modalStep1.style.display = 'none';
+			modalStep2.style.display = 'block';
+			modalNext.setAttribute('disabled', 'disabled');
+		}
+	}
+	function getElemStyle( elem, style ) {
+		var value = '';
+		if ( elem.style[style] !== '' ) {
+			value = elem.style[style];
+		} else {
+			value = elem.currentStyle ? elem.currentStyle[style] : getComputedStyle( elem, null )[style];
+		}
+		return value;
 	}
 	function toggleResourceRights(e) {
 		var target = e.target;
@@ -56,29 +138,28 @@
 			modalNext.removeAttribute('disabled');
 		});
 	}
-	modalDialog.addEventListener( 'transitionend', function(e) {
-		modalOpen = !modalOpen;
-		if ( !modalOpen ) {
-			modal.style.display = 'none';
-			modalBg.style.display = 'none';
-		}
-	});
 	function toggleModal(show) {
 		if (show) {
 			modal.style.display = 'block';
 			modalBg.style.display = 'block';
 			setTimeout( function() {
-				modal.classList.add('in');
-				modalBg.classList.add('in');
+				modal.classList.add('shown');
+				modalBg.classList.add('shown');
 			}, 0);
 		} else {
-			modal.classList.remove('in');
-			modalBg.classList.remove('in');
+			modal.classList.remove('shown');
+			modalBg.classList.remove('shown');
 		}
 	}
 	function resize() {
 		folderList.style.height = (global.innerHeight - folderList.offsetTop) +'px';
 		cardPane.style.height = (global.innerHeight - cardPane.offsetTop) +'px';
+		modalScrolling();
+	}
+	function modalScrolling() {		
+		modalBody.scrollTop > 0 ? modalHeader.classList.add('scrolled') : modalHeader.classList.remove('scrolled');
+		var innerHeight = modalBody.clientHeight - parseInt( getElemStyle(modalBody, 'paddingTop'), 0) - parseInt( getElemStyle(modalBody, 'paddingBottom'), 0);
+		modalBody.scrollHeight > modalBody.clientHeight ? modalFooter.classList.add('scrolled-inverse') : modalFooter.classList.remove('scrolled-inverse');
 	}
 	function scrolling(e) {
 		var elem = e.target;
@@ -106,10 +187,9 @@
 	global.onresize = debounce( resize, delay );
 	folderList.onscroll = scrolling;
 	cardPane.onscroll = scrolling;
-
 	global.setTimeout(resize, 250);
-	global.setTimeout( function() {
-		toggleModal(true);
-	}, 1000);
+	// global.setTimeout( function() {
+	// 	toggleModal(true);
+	// }, 1000);
 
 })( window )
